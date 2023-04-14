3d08942f78dbc5006e3cbcee12481fa0fe645bcf
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-14 07:19:43 +0300

    вторая задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт какое число больше, а какое меньшее.
+// План действий:
+//  1)Запросить у пользователя число командой Write.
+//  2)Поставить цикл и ввести переменную на его число.
+//  3)Запомнить это число.
+//  4) Дальше поставить цикл на второй запрос числа.
+//  5) Запомнить это число.
+//  6) Сравнить два эти числа.
+
+Console.Write("Введите число a: ");
+string s = Console.ReadLine();
+
+if (int.TryParse(s, out int i))
+{
+    Console.Write("Введите число b: ");
+    string c = Console.ReadLine();
+
+    if (int.TryParse(c, out int j))
+
+    {
+        if (i > j){
+            Console.WriteLine(s);
+        }
+        else{
+            Console.WriteLine(c);
+        }
+    }
+
+    else
+    {
+        Console.WriteLine("Ошибка при введение числа");
+    }
+
+}
+else
+{
+    Console.WriteLine("Ошибка при введение числа");
+}
+
+
+//Console.Write("Введите число a: ");
+//int i = int.Parse(Console.ReadLine());
+
+//Console.Write("Введите число b: ");
+//int j = int.Parse(Console.ReadLine());
+
+//if (i > j){
+   // Console.WriteLine(i);
+//}
+//else{
+   // Console.WriteLine(j);
+//}
---
+++ Task_4/Program.cs
@@ -0,0 +1,63 @@
+﻿//  Написать программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+//  План действий:
+//  1. Запросить у пользователя число
+//  2. Проверить его на TryParse
+//  3. Повторить те же действия на ещё два числа.
+//  4. Запомнив эти числа, дав им имя переменной, сравнить их посредством числа max = 0
+//  5. Вывести максимальное число
+
+int max = 0;
+
+Console.Write("Введите число a: ");
+string s = Console.ReadLine();
+
+if (int.TryParse(s, out int i))
+{
+    Console.Write("Введите число b: ");
+    string c = Console.ReadLine();
+
+    if (int.TryParse(c, out int j))
+
+    {
+        Console.Write("Введите число с: ");
+        string o = Console.ReadLine();
+        
+        if (int.TryParse(o, out int y))
+
+        {
+            if (max <= i)
+            {
+                max = i;
+            }
+
+            if (max <= j)
+            {
+                max = j;
+            }
+
+            if (max <= y)
+            {
+                max = y;
+            }
+            
+            Console.WriteLine("Максимальное число: " + max);
+        }
+
+        else
+        {
+            Console.WriteLine("Ошибка при вводе числа");
+        }
+
+
+    }
+
+    else
+    {
+        Console.WriteLine("Ошибка при вводе числа");
+    }
+
+}
+else
+{
+    Console.WriteLine("Ошибка при вводе числа");
+}
